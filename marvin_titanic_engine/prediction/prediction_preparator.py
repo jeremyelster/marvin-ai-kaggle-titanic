72001a65a7bcc18f6396039a6ddce1673f8ba954
Jeremy Elster <jeremy.elster@b2wdigital.com>
2018-03-29 17:42:45 +0000

    Updated params and prediction preparator to allow a named dict to make clear what the variables are

--- marvin_titanic_engine/prediction/prediction_preparator.py
+++ marvin_titanic_engine/prediction/prediction_preparator.py
@@ -23,8 +23,11 @@ class PredictionPreparator(EngineBasePrediction):
         super(PredictionPreparator, self).__init__(**kwargs)
 
     def execute(self, input_message, params, **kwargs):
-        """
-        Return a prepared input_message compatible to the predict algorithm used by the model.
-        Use the self.model and self.metrics objects if necessary.
-        """
+        # put this values in engine.messages to be used as dryrun samples
+        # age, class, sex
+        input_message = {"age": 50, "class": 3, "sex": 0}
+
+        key_order = {"age": 0, "class": 1, "sex": 2}
+        input_message = [input_message[i] for i in sorted(input_message, key=key_order.__getitem__)]
+
         return input_message
